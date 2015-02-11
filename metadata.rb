2fe68719b35f543a1199debccf5a9bcd7aba680b
Holger Protzek <holger.protzek@springer.com>
2015-02-11 23:50:20 +0100

    fixed typo

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description       'Installs and configures Nagios NRPE client'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.1.0'
 
-centos
+supports          'centos'
